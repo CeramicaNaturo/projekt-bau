8fbeb058ec577b7bda2763a5e5b069cbaf479188
CeramicaNaturo <orucv1975@gmail.com>
2026-08-07 16:28:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-
-const CACHE='tadilat-mini-v01';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='projekt-bau-v02',A=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(x=>x.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
